e00a430921ee0ec26ff16e5c40d27d4c8ae1d8ce
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-19 03:46:55 +0100

    error redirect

--- src/components/start.py
+++ src/components/start.py
@@ -78,3 +78,4 @@ def manage_groups(update: Update, context: CallbackContext):
 
 def unsupported(update: Update, context: CallbackContext):
     update.effective_message.reply_text("Unsupported type of media to send.")
+    return start(update, context)
