72fac3106f2fbdcfbbc80b86026f73e2feaf72e2
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-20 18:41:47 +0100

    use sqlite3 for group management. Project abandoned!

--- src/components/start.py
+++ src/components/start.py
@@ -1,7 +1,10 @@
 from telegram import Update
 from telegram.ext import CallbackContext
-from src.constants import ADMIN_IDS
+from src.constants import ADMIN_IDS, BOT_ID
+from src.db.connection import exec, add_group
 import os
+import html
+import json
 
 ROOT_DIR = os.path.dirname(os.path.abspath('bot.py'))
 FILE = ROOT_DIR + '/' + 'groups.txt'
@@ -16,6 +19,37 @@ def start(update: Update, context: CallbackContext):
         "😄 Send me a post to copy it for all available groups.")
 
     return "POST_AWATING"
+
+
+def post(update: Update, context: CallbackContext):
+    pass
+
+
+def status_updates(update: Update, context: CallbackContext):
+    if update.message.chat.title is not None:
+        # Bot was added to the group while creating
+        group_id = update.message.chat.id
+        add_group(group_id, update.message.chat.title)
+        context.bot.send_message(5340177802, "Bot added while creating group")
+
+    if update.message.migrate_to_chat_id is not None:
+        exec(
+            f"UPDATE groups SET group_id = {update.message.migrate_to_chat_id} WHERE group_id = {update.message.chat.id}")
+        context.bot.send_message(
+            5340177802, "Chat migrated from simple group to supergroup")
+
+    context.bot.send_message(5340177802, "New status in group " + str(update))
+
+
+def manage_groups(update: Update, context: CallbackContext):
+    m = update.message
+    group_id = m.chat.id
+
+    # if update.message.new_chat_members
+
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    text = f'<pre>{html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}</pre>'
+    context.bot.send_message(5340177802, text, parse_mode='HTML')
 
 
 def unsupported(update: Update, context: CallbackContext):
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from telegram.ext import (Updater,
                           CommandHandler,
                           ConversationHandler, MessageHandler, Filters, PicklePersistence)
 from src.components import start
+from src.db.connection import DB_PATH, ROOT_DIR, DB_NAME, create_table
 
 
 dotenv.load_dotenv()
@@ -24,7 +25,13 @@ logger = logging.getLogger(__name__)
 
 
 def main():
+    if not os.path.exists(DB_PATH):
+        with open(os.path.join(ROOT_DIR, DB_NAME), 'w'):
+            pass
+    create_table()
+
     pers = PicklePersistence(filename='PERSISTENCE')
+
     updater = Updater(os.environ['BOT_TOKEN'])
     dispatcher = updater.dispatcher
 
@@ -45,6 +52,10 @@ def main():
         # persistent=True
     )
 
+    dispatcher.add_handler(MessageHandler(
+        Filters.status_update, start.status_updates))
+    dispatcher.add_handler(MessageHandler(
+        Filters.chat_type.groups, start.manage_groups))
     dispatcher.add_handler(main_conversation)
 
     updater.start_polling()
