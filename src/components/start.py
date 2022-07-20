555aa24857972269a8808991b40fbb9f1fd026a2
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-07-20 13:20:26 +0100

    remove useless shit code

--- src/components/start.py
+++ src/components/start.py
@@ -1,10 +1,6 @@
-from email import message
-from token import AT
 from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.error import Unauthorized, BadRequest
-from utils.text import text
-from utils.get_groups import get_groups
 from src.constants import ADMIN_IDS, BOT_ID
 import os
 import time
@@ -22,68 +18,6 @@ def start(update: Update, context: CallbackContext):
         "😄 Send me a post to copy it for all available groups.")
 
     return "POST_AWATING"
-
-
-def post(update: Update, context: CallbackContext):
-    message = update.effective_message
-    group_ids = get_groups(FILE)
-
-    if group_ids is 1 or len(group_ids) == 0:
-        update.effective_message.reply_text("There are no groups to send.")
-        return
-
-    text = f"Wait... I started delivering your message to all groups. Progress: 0/{len(group_ids)}"
-    m = update.effective_message.reply_text(text)
-
-    not_sent = 0
-    for i in group_ids:
-        try:
-            copied_m = message.copy(i)
-            context.bot.pin_chat_message(i, copied_m.message_id)
-            m.edit_text(
-                f"Wait... I started delivering your message to all groups. Progress: {group_ids.index(i) + 1}/{len(group_ids)}")
-            time.sleep(3)
-        except (Unauthorized, BadRequest) as e:
-            not_sent += 1
-            continue
-
-    update.effective_message.reply_text(
-        f"Post has been sent to all authorized groups: <b>{len(group_ids) - not_sent}/{len(group_ids)}</b>", parse_mode='HTML')
-    return start(update, context)
-
-
-def manage_groups(update: Update, context: CallbackContext):
-
-    group_id = str(update.effective_chat.id)
-    file = 'groups.txt'
-
-    # New group
-    if not os.path.exists(FILE):
-        with open(file, 'w') as new_file:
-            new_file.write(group_id + '\n')
-            return
-
-    groups_list = []
-    with open(file, 'r') as f:
-        lines = f.readlines()
-        for i in lines:
-            groups_list.append(i.replace("\n", ""))
-
-    try:
-        left = update.message.left_chat_member
-        if left.id == BOT_ID:
-            with open(file, 'w') as fp:
-                for line in lines:
-                    if line.strip("\n") != group_id:
-                        fp.write(line)
-        return
-    except AttributeError:
-        pass
-
-    if group_id not in groups_list:
-        with open(file, 'a') as ap_file:
-            ap_file.write(group_id + '\n')
-            return
 
 
 def unsupported(update: Update, context: CallbackContext):
